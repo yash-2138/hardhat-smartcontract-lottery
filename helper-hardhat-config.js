dcb26831a865e8c9e8dbf1ffeb098d1cb7a6c0e6
AN ❦ <108299225+krakxn@users.noreply.github.com>
2022-08-15 01:54:21 +0530

    Update helper-hardhat-config.js

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,18 +1,22 @@
 const { ethers } = require("hardhat")
 
 const networkConfig = {
+    default: {
+        name: "hardhat",
+        keepersUpdateInterval: "30",
+    },
     4: {
         name: "rinkeby",
         vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
         entranceFee: ethers.utils.parseEther("0.01"),
         gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
-        subscriptionId : "0",
+        subscriptionId : "0", // ad a subscriptionId here 
         callBackGasLimit: "500000",
         interval: "30",
     },
 
     31337:{
-        name: "hardhat",
+        name: "localhost",
         //do not need vrfCoordinatorV2 as we are deploying a mock
         vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
         entranceFee: ethers.utils.parseEther("0.01"),
